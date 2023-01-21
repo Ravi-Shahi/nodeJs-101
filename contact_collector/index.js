6ec4d98d8e7ba790b8ac8058740f73fd79da2b83
Ravi Shahi <ravishahi1810@gmail.com>
2023-01-21 22:35:19 +0530

    api field parameters issue

--- contact_collector/index.js
+++ contact_collector/index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
+
+
+mailchimp.setConfig({
+    apiKey: process.env.MAIL_CHIMP_API_KEY,
+    server: process.env.SERVER_PREFIX
+});
+const data = mailchimp.ping.get();
+setTimeout(() => console.log(data), 2000);
+
+
+
 
 const app = express();
 app.use(express.static('public'));
@@ -11,13 +23,28 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-    const fname = req.body.fname;
-    const lname = req.body.lname;
+    const firstName = req.body.fname
+    const lastName = req.body.lname;
     const email = req.body.email;
-    console.log(req);
+    const campaignDefaults = {
+        from_name: "Gettin' Together",
+        from_email: "gettintogether@example.com",
+        subject: "JS Developers Meetup",
+        language: "EN_US"
+    };
+    async function add() {
+        const response = await mailchimp.lists.createList({
+            name: "Dairy",
+            contact: email,
+            permission_reminder: "permission_reminder",
+            email_type_option: true,
+            campaign_defaults: campaignDefaults
+        })
+        console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}`);
+    };
+    add();
+})
 
-    console.log(fname + lname + ':' + email);
-});
 
 
 
