950040faaf9cc6fcbde49764928d4ef0f9435fdc
Ravi Shahi <ravishahi1810@gmail.com>
2023-01-22 23:37:19 +0530

    created app to save user info to mail-chimp

--- contact_collector/index.js
+++ contact_collector/index.js
@@ -9,11 +9,6 @@ mailchimp.setConfig({
     apiKey: process.env.MAIL_CHIMP_API_KEY,
     server: process.env.SERVER_PREFIX
 });
-const data = mailchimp.ping.get();
-setTimeout(() => console.log(data), 2000);
-
-
-
 
 const app = express();
 app.use(express.static('public'));
@@ -26,26 +21,24 @@ app.post('/', function(req, res) {
     const firstName = req.body.fname
     const lastName = req.body.lname;
     const email = req.body.email;
-    const campaignDefaults = {
-        from_name: "Gettin' Together",
-        from_email: "gettintogether@example.com",
-        subject: "JS Developers Meetup",
-        language: "EN_US"
-    };
-    async function add() {
-        const response = await mailchimp.lists.createList({
-            name: "Dairy",
-            contact: email,
-            permission_reminder: "permission_reminder",
-            email_type_option: true,
-            campaign_defaults: campaignDefaults
-        })
-        console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}`);
-    };
-    add();
+    const data = {
+        email_address: email,
+        merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+        },
+        status: "subscribed"
+    }
+    mailchimp.lists.addListMember(process.env.LIST_ID, data)
+        .then((response) => {
+            console.log("Successfully Subscribed!")
+            res.sendFile(__dirname + '/successful.html');
+        }, (reject) => {
+            console.log(reject.response.body.title + " Please try Again!")
+            res.sendFile(__dirname + "/failure.html");
+        });
+
 })
-
-
 
 
 app.listen(process.env.PORT, function() {
