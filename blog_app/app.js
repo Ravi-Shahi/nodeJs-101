887718b865d3d50bb33f6b87020d4efdca4bb732
Ravi Shahi <ravishahi1810@gmail.com>
2023-01-28 23:31:36 +0530

    resolved url space issue

--- blog_app/app.js
+++ blog_app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,6 +18,14 @@ app.get('/', (req, res) => {
     res.render('home', {
         homeContent: homeStartingContent,
         posts: posts
+    })
+})
+app.get('/post/:postName', (req, res) => {
+    let requestedTitle = _.lowerCase(req.params.postName);
+    posts.forEach((post) => {
+        let storedTitle = _.lowerCase(post.title);
+        if (storedTitle == requestedTitle)
+            console.log('match found');
     })
 })
 app.get('/about', (req, res) => {
