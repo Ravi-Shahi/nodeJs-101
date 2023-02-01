225024371477dd1db48324e6ef94781fb43b3de5
Ravi Shahi <ravishahi1810@gmail.com>
2023-02-01 19:21:57 +0530

    initial conection to database

--- to-do_list/app.js
+++ to-do_list/app.js
@@ -1,3 +1,4 @@
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,6 +8,26 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+
+    // the following code examples can be pasted here...
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
 
 const date = new Date();
 const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
