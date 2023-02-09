b1229b88a6d5f2be23ff3657a8d533726b02b331
Ravindar Kumar Shahi <ravishahi1810@gmail.com>
2023-02-09 19:54:44 +0530

    delete operation

--- to-do_list/app.js
+++ to-do_list/app.js
@@ -17,7 +17,7 @@ const itemSchema = new mongoose.Schema({
   name: String
 })
 
-const item = new mongoose.model("items",itemSchema);
+const item = new mongoose.model("items", itemSchema);
 
 const item1 = new item({
   name: "Welcome to todo list App"
@@ -31,33 +31,45 @@ const item3 = new item({
   name: "Hit <-- this to delete item"
 })
 
-const item4 = [item1,item2,item3];
+const item4 = [item1, item2, item3];
 
 const date = new Date();
 const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const today = date.toLocaleDateString('hi-IN', dateOptions);
 
 app.get('/', (req, res) => {
-  item.find({},(err,result)=>{
-    if(!err){
-      if(result.length === 0){
+  item.find({}, (err, result) => {
+    if (!err) {
+      if (result.length === 0) {
         item.insertMany(item4);
         res.redirect('/')
-      }else{
+      } else {
         res.render('index', { day: today, newItem: result });
       }
     }
-    }
+  }
   )
-  
+
 });
 
 app.post('/', (req, res) => {
   const newItem = new item({
     name: req.body.newItem
   });
+  console.log(req.body);
   newItem.save();
   res.redirect('/');
+})
+
+app.post('/delete',(req,res)=>{
+  item.findByIdAndRemove(req.body.checkbox, (err,response)=>{
+    if(!err){
+      res.redirect('/')
+    }else{
+      console.log(err)
+    }
+  })
+
 })
 
 
